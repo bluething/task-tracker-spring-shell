1e4940eda59db0a57ab3e747607094be1009e812
Habib Ali Machpud <habib.machpud@gmail.com>
2024-09-07 23:48:37 +0700

    Register the command

--- src/main/java/io/github/bluething/playground/spring/boot/tasktracker/TasktrackerApplication.java
+++ src/main/java/io/github/bluething/playground/spring/boot/tasktracker/TasktrackerApplication.java
@@ -2,12 +2,14 @@ package io.github.bluething.playground.spring.boot.tasktracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.shell.command.annotation.EnableCommand;
 
 @SpringBootApplication
-public class TasktrackerApplication {
+@EnableCommand(TaskCommands.class)
+public class TaskTrackerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TasktrackerApplication.class, args);
+		SpringApplication.run(TaskTrackerApplication.class, args);
 	}
 
 }
