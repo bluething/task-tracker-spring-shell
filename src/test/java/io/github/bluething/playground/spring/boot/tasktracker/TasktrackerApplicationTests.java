710a162cb224d4bee43333f55481620e2384d8dc
Habib Ali Machpud <habib.machpud@gmail.com>
2024-09-07 15:45:31 +0700

    init

---
+++ src/test/java/io/github/bluething/playground/spring/boot/tasktracker/TasktrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.bluething.playground.spring.boot.tasktracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasktrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
